585ca5b6940d921b5d04d027cf824bb5fd33ca62
Kenneth Petiote <kenneth.petiote@gmail.com>
2021-07-15 18:27:01 -0400

    Modified  models/books.js

--- models/books.js
+++ models/books.js
@@ -1,2 +1,36 @@
 // BOOKS JS
 // ---------------------------------------------------------------------------
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    authors: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        unique: true
+    },
+    link: {
+        type: String,
+        required: true
+    }
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
