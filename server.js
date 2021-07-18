7e4e05e905aceb45dcee3c1fbd9fc670d0587d6e
Kenneth Petiote <Ken@Kens-iMac.local>
2021-07-18 11:33:51 -0400

    Modified server.js

--- server.js
+++ server.js
@@ -1,2 +1,28 @@
 // SERVER JS
 // ---------------------------------------------------------------------------
+
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 3001;
+const routes = require("./routes");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+app.use(routes);
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/googlebooks";
+
+mongoose.connect(MONGODB_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+});
+
+app.listen(PORT, () => {
+    console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
