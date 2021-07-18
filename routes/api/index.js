24fd117dc42a8551bf05d8ec0dfb9050f636ea8e
Kenneth Petiote <Ken@Kens-iMac.local>
2021-07-18 09:27:40 -0400

    Modified routes/api/index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,2 +1,9 @@
 // INDEX JS
 // ---------------------------------------------------------------------------
+
+const router = require("express").Router();
+const bookRoutes = require("./books");
+
+router.use("/books", bookRoutes);
+
+module.exports = router;
