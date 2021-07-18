1bb7c86b905ca367d61d865aca7357762463712f
Kenneth Petiote <Ken@Kens-iMac.local>
2021-07-18 11:03:23 -0400

    Modified client/src/components/SaveButton/SaveButton.js

--- client/src/components/SaveButton/SaveButton.js
+++ client/src/components/SaveButton/SaveButton.js
@@ -1,2 +1,12 @@
 // SAVE BUTTON JS
 // ---------------------------------------------------------------------------
+
+import React from 'react'
+
+export default function SaveButton() {
+    return (
+        <div>
+
+        </div>
+    )
+}
