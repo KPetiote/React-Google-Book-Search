0f2f3a246f7d8d8456d169b81249701f2c98b056
Kenneth Petiote <Ken@Kens-iMac.local>
2021-07-18 11:25:20 -0400

    Modified client/src/pages/Saved.js

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,2 +1,67 @@
 // SAVED JS
 // ---------------------------------------------------------------------------
+
+import React, { Component } from 'react'
+import { Container } from "../components/Grid/Grid";
+import Nav from "../components/Nav/Nav";
+import Jumbotron from "../components/Jumbotron/Jumbotron";
+import API from '../utils/API';
+import SavedCard from "../components/SavedCard/SavedCard";
+
+class Saved extends Component {
+
+    state = {
+        savedBooks: []
+    }
+
+    componentDidMount = () => {
+        this.getBooks()
+    }
+
+    deleteGoogleBook = currentBook => {
+        API.deleteGoogleBook( currentBook.id )
+        .then(res => {
+            console.log("You have deleted this book:", res);
+            this.viewSavedGoogleBooks();
+        })
+        .catch(err => {
+            console.log("This is the error", err);
+        })
+    }
+
+    getBooks = () => {
+        API.viewSavedGoogleBooks()
+        .then(res => {
+            this.setState({
+                savedBooks: res.data
+            })
+            console.log("This is the res from getBooks", res);
+        })
+        .catch(err => {
+            console.log("This is the error", err);
+        })
+    }
+
+
+    render() {
+        return (
+            <div>
+                <Nav />
+                <Container fluid>
+                <Jumbotron />
+                {this.state.savedBooks.length ? (
+                    <SavedCard 
+                    bookState={this.state.savedBooks}
+                    deleteGoogleBook={this.deleteGoogleBook}
+                    >
+                    </SavedCard>
+                ) : (
+                    <h5>No saved books</h5>
+                )}
+                </Container>
+            </div>
+        )
+    }
+}
+
+export default Saved
