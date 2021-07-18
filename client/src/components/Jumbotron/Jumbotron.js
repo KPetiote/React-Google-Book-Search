e733cf6297e74d6193e5af252b1832d633a52898
Kenneth Petiote <Ken@Kens-iMac.local>
2021-07-18 10:46:45 -0400

    Modifid  client/src/components/Jumbotron/Jumbotro.js

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -1,2 +1,19 @@
 // JUMBOTRON JS
 // ---------------------------------------------------------------------------
+
+import React from 'react';
+
+function Jumbotron() {
+    return (
+        <div>
+            <div className="jumbotron">
+                <h1 className="display-4 text-center">Google Book Search</h1>
+                <br></br>
+                <p className="text-center">Use the search field below to find books from out database.</p>
+                <p className="text-center">You have the option to save the books to read at a later date.</p>
+            </div>
+        </div>
+    )
+}
+
+export default Jumbotron;
