c5e8367b98eebff628c2bcce9cfd601afbecc037
Kenneth Petiote <Ken@Kens-iMac.local>
2021-07-18 09:51:22 -0400

    Modified client/src/utils/API.js

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,2 +1,29 @@
 // API JS
 // ---------------------------------------------------------------------------
+
+// Required module
+import axios from "axios";
+const GOOGLEURL = "https://www.googleapis.com/books/v1/volumes?q=";
+
+export default {
+    // Retrieve Google books
+    getGoogleBooks: function(query) {
+        return axios.get(GOOGLEURL + query)
+    },
+    // Saves book to DB
+    saveGoogleBook: function(bookData) {
+        return axios.post("/api/books", bookData);
+    },
+    // View book with the given ID
+    viewSingleGoogleBook: function(id) {
+        return axios.get("/api/books/" + id);
+    },
+    // View all saved books from DB
+    viewSavedGoogleBooks: function() {
+        return axios.get("/api/books");
+    },
+    // Deletes book with the given ID
+    deleteGoogleBook: function(id) {
+        return axios.delete("/api/books/" + id);
+    }
+};
