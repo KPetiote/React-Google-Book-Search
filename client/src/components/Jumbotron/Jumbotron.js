84cd9f5325bfdc6f10ac9f4482d036d08e92be4d
Kenneth Petiote <Ken@Kens-iMac.local>
2021-07-19 08:26:04 -0400

    Updated Icon

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -10,7 +10,7 @@ function Jumbotron() {
             <div className="jumbotron">
                 <h1 className="display-4 text-center jumbotronText">Google Book Search</h1>
                 <br></br>
-                <h2 className="text-center jumbotronText">Search and save books of your interest.</h2>
+                <h2 className="text-center jumbotronText">Search and save books from the Google Database</h2>
             </div>
         </div>
     )
